2a478605f1934f2f98ef4291bdd86313a0fe5baa
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-04 15:05:35 -0300

    OrderStatus enum

--- course/src/main/java/com/CustomerOrderManager/course/entities/Order.java
+++ course/src/main/java/com/CustomerOrderManager/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.CustomerOrderManager.course.entities;
 
+import com.CustomerOrderManager.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,6 +15,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -22,11 +24,12 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -51,6 +54,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
