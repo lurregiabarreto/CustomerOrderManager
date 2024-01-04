0e034ec54efa1ab5f04111b09024ec83fd36bf5c
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-04 15:26:20 -0300

    Category

---
+++ course/src/main/java/com/CustomerOrderManager/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.CustomerOrderManager.course.services;
+
+import com.CustomerOrderManager.course.entities.Category;
+import com.CustomerOrderManager.course.repositories.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
