13bddad23f2e0dc4689ff417355e4cc843e88879
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-04 15:36:56 -0300

    Product

---
+++ course/src/main/java/com/CustomerOrderManager/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.CustomerOrderManager.course.repositories;
+
+import com.CustomerOrderManager.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
