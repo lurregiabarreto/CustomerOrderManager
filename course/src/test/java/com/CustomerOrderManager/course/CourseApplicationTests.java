23b694d9a339aac461588985bc3f9b1352132ef6
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-04 10:15:59 -0300

    Project created

---
+++ course/src/test/java/com/CustomerOrderManager/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CustomerOrderManager.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
