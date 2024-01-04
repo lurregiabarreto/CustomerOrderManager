4820fb2c8f8e6759fbb8614ec7b7602756ce4e27
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-04 14:44:40 -0300

    Order, Instant, ISO 8601 - PART 1

--- course/src/main/java/com/CustomerOrderManager/course/entities/User.java
+++ course/src/main/java/com/CustomerOrderManager/course/entities/User.java
@@ -3,6 +3,8 @@ package com.CustomerOrderManager.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
@@ -27,6 +32,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
